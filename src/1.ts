b19885beb63c99485d6b2779fd22a1a11b7c91b6
pinkneupenni <pinkneupenni@gmx.com>
2025-03-03 14:24:03 +0000

    Add 1.ts

---
+++ src/1.ts
@@ -0,0 +1,6 @@
+const getRandomMathProblem = () => {
+  const numbers = [1, 2, 3, 4, 5];
+  const operators = ["+", "-", "*", "/"];
+  const problem = `${numbers[0]} ${operators[0]} ${numbers[1]}`;
+  return problem;
+}
